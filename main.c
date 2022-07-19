7a9f25d0e1a7ebe54a072b99a7b20364adb21c99
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-19 17:46:16 +0800

    First commit

---
+++ main.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h> // To use exit(0) function
+
+/* Structs */
+// typedef keyword to create an alias name for data types. It is commonly used with structures to simplify the syntax of declaring variables.
+typedef struct User
+{
+    int userLevel; // userLevel, 1 = admin, 2 = user
+} user;
+
+typedef struct Movie
+{
+    char movieName[50];
+    float movieFee;
+} movie;
+
+/* User-defined functions */
+int welcomeMessage()
+{
+    // Login
+    int loginInput;
+    printf("----------------------------------------------------------------------\n");
+    printf("Welcome to Golden Screen Cinema Ticket Booking System!\n");
+    printf("Choose your input:\n");
+    printf("1. Login as an admin\n");
+    printf("2. Login as a user\n");
+    printf("3. Exit\n");
+    printf("----------------------------------------------------------------------\n");
+    scanf("%d", &loginInput);
+    return loginInput;
+}
+
+int userFunction(int input)
+{
+    if (input == 1)
+    {
+        // To purchase ticket
+    }
+    else if (input == 2)
+    {
+        // To cancel the seat
+    }
+    else if (input == 3)
+    {
+        // Exit
+        printf("See you soon!\n");
+        exit(0);
+    }
+}
+
+int adminFunction(int input)
+{
+    if (input == 1)
+    {
+        // View the reserved tickets
+    }
+    else if (input == 2)
+    {
+        // Edit the price of ticket
+    }
+    else if (input == 3)
+    {
+        // Exit
+        printf("See you soon!\n");
+        exit(0);
+    }
+}
+
+/* Main function */
+int main()
+{
+    int loginInput = welcomeMessage();
+
+    int input;
+    if (loginInput == 1) // admin
+    {
+        printf("Welcome admin!\n");
+        printf("Indicate your preference by entering the appropriate number:\n");
+        printf("1. View the reserved tickets\n");
+        printf("2. Edit the price of ticket\n");
+        printf("3. Exit\n");
+        scanf("%d", &input);
+
+        adminFunction(input);
+    }
+    else if (loginInput == 2) // user
+    {
+        printf("Welcome user!\n");
+        printf("Please indicate your preference by entering the appropriate number:\n");
+        printf("1. To purchase ticket\n");
+        printf("2. To cancel the seat\n");
+        printf("3. Exit\n");
+        scanf("%d", &input);
+
+        userFunction(input);
+    }
+    else if (loginInput == 3)
+    {
+        printf("See you soon!\n");
+        exit(0);
+    }
+    return 0;
+}
