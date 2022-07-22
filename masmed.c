27a072da2eec55db4846ab9825161d8587cb7762
HaziqKhairi <ikhairulhaziq11@gmail.com>
2022-07-22 14:24:37 +0800

    Update

--- masmed.c
+++ masmed.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
-// function to calculate subtotal
+struct menu
+{
+    char menuName[64];
+    float menuPrice;
+};
+
+// Function to calculate subtotal
 float totalPrice(float subTotal, float tax)
 {
-    float bill_Amount = subTotal + tax;
-    return bill_Amount;
+    float billAmount = subTotal + tax;
+    return billAmount;
 }
 
-// function to calculate tax
+// Function to calculate tax (Govt Tax)
 float taxCalc(float subTotal, float taxRate)
 {
     float tax = (subTotal * taxRate) / 100;
     return tax;
 }
 // main program
-int main(void)
+int main()
 {
-    //	Local Declarations
-    const double nachos = 5.00, lekor = 2.00, lekorL = 3.50, nasi = 4.50, burger = 3.50, drink = 2.00, drinkL = 3.00, taxRate = 0.6;
+
+    // Write to txt file
+    struct menu menu[7];
+    strcpy(menu[0].menuName, "Nachos");
+    menu[0].menuPrice = 5.00;
+    strcpy(menu[1].menuName, "Keropok Lekor (Small)");
+    menu[1].menuPrice = 2.00;
+    strcpy(menu[2].menuName, "Keropok Lekor (Large)");
+    menu[2].menuPrice = 3.50;
+    strcpy(menu[3].menuName, "Nasi Lemak");
+    menu[3].menuPrice = 4.50;
+    strcpy(menu[4].menuName, "Burger");
+    menu[4].menuPrice = 3.50;
+    strcpy(menu[5].menuName, "Drink (Small)");
+    menu[5].menuPrice = 2.00;
+    strcpy(menu[6].menuName, "Drink (Large)");
+    menu[6].menuPrice = 3.00;
+
+    FILE *fptr;
+
+    fptr = fopen("./menu.txt", "w");
+
+    if (fptr == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+
+    for (int x = 0; x < sizeof(menu) / sizeof(*menu); x++)
+    {
+        fprintf(fptr, "%s\n%.2f\n", menu[x].menuName, menu[x].menuPrice);
+        fprintf(fptr, "\n"); // New line
+    }
+
+    fclose(fptr);
+
+    /*	Local Declarations */
+    // Fixed food prices
+    const double
+        nachos = 5.00,
+        lekor = 2.00,
+        lekorL = 3.50,
+        nasi = 4.50,
+        burger = 3.50,
+        drink = 2.00,
+        drinkL = 3.00;
+
+    // Tax rate in %
+    const double taxRate = 6;
     int ncQty, lekorQty, nlQty, burgerQty, drinkQty, drinkChoice, lekorChoice;
     float subTotal;
     char name[250], cust[250], userChoice = 'y';
@@ -27,15 +82,15 @@ int main(void)
     //	Statements
     while (userChoice == 'y')
     {
-        printf("\t\tWelcome To MASMED Sales Billing System\n");
-        printf("\n Please Enter the following details. \n\n");
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\nWelcome To MASMED Sales Billing System\n");
+        printf("Please enter the following details:\n\n");
 
-        printf("Cashier's Name: \n");
+        printf("Cashier's name: \n");
         scanf("%s", name);
-        printf("Customer's Name: \n");
+        printf("Customer's name: \n");
         scanf("%s", cust);
 
-        printf("\n\t***Enter 0 for none****");
+        printf("\n*** Enter 0 (quantity) if none were sold ****");
 
         printf("\nHow many nachos were sold? \n");
         scanf("%d", &ncQty);
@@ -43,8 +98,10 @@ int main(void)
 
         printf("How many keropok lekors were sold? \n");
         scanf("%d", &lekorQty);
+
         printf("What type of lekor were sold? (1 for small, 2 for large) \n");
         scanf("%d", &lekorChoice);
+
         if (lekorChoice == 1)
             total[1] = lekorQty * lekor;
         else if (lekorChoice == 2)
@@ -72,44 +129,43 @@ int main(void)
         {
             subTotal += total[i];
         }
-        // calling function to calculate food price
+        // Calling function to calculate food price
         float tax = taxCalc(subTotal, taxRate);
         float finalPrice = totalPrice(subTotal, tax);
 
         //	Printing Bills
         printf("\n");
-        printf("\t\t\t====================================================\n");
-        printf("\t\t\t\t          Masmed Sales Bill\n");
-        printf("\t\t\t====================================================\n");
-        printf("\t\t\tQTY \t Description \t Unit Price$\t Total Price$\n");
-        printf("\t\t\t--- \t ----------- \t ---------- \t -----------\n\n");
-        printf("\t\t\t%02d \t Nachos                %.2f       %10.2f \n", ncQty, nachos, total[0]);
+        printf("\t\t\t================================================================\n");
+        printf("\t\t\t\t\t\t*Masmed Sales Bill*\n\n");
+        printf("\t\t\t\t\tCustomer - %s     \n", cust);
+        printf("\t\t\t\t\tCashier  - %s     \n", name);
+        printf("\t\t\t================================================================\n");
+        printf("\t\t\tQty \t Description \t Unit Price(RM) \t Total Price(RM)\n");
+        printf("\t\t\t--- \t ----------- \t -------------- \t ---------------\n\n");
+        printf("\t\t\t%02d \t Nachos                    %.2f              %10.2f \n", ncQty, nachos, total[0]);
 
         if (lekorChoice == 1)
-            printf("\t\t\t%02d  \t Keropok Lekor(S)      %.2f       %10.2f  \n", lekorQty, lekor, total[1]);
+            printf("\t\t\t%02d  \t Keropok Lekor(S)          %.2f              %10.2f  \n", lekorQty, lekor, total[1]);
         else if (lekorChoice == 2)
-            printf("\t\t\t%02d  \t Keropok Lekor(L)      %.2f       %10.2f  \n", lekorQty, lekorL, total[1]);
+            printf("\t\t\t%02d  \t Keropok Lekor(L)          %.2f              %10.2f  \n", lekorQty, lekorL, total[1]);
 
-        printf("\t\t\t%02d   \t Nasi Lemak            %.2f       %10.2f   \n", nlQty, nasi, total[2]);
-        printf("\t\t\t%02d    \t Burger                %.2f       %10.2f    \n", burgerQty, burger, total[3]);
+        printf("\t\t\t%02d   \t Nasi Lemak                %.2f              %10.2f   \n", nlQty, nasi, total[2]);
+        printf("\t\t\t%02d    \t Burger                    %.2f              %10.2f    \n", burgerQty, burger, total[3]);
 
         if (drinkChoice == 1)
-            printf("\t\t\t%02d     \t Drinks(S)             %.2f       %10.2f     \n", drinkQty, drink, total[4]);
+            printf("\t\t\t%02d     \t Drinks(S)                 %.2f              %10.2f     \n", drinkQty, drink, total[4]);
         else if (drinkChoice == 2)
-            printf("\t\t\t%02d     \t Drinks(L)             %.2f       %10.2f     \n", drinkQty, drinkL, total[4]);
+            printf("\t\t\t%02d     \t Drinks(L)                 %.2f              %10.2f     \n", drinkQty, drinkL, total[4]);
 
-        printf("\t\t\t                                           ---------\n\n");
-        printf("\t\t\t                           SUBTOTAL       %10.2f   \n", subTotal);
-        printf("\t\t\t                           TAX            %10.2f    \n", tax);
-        printf("\t\t\t                           Total$         %10.2f     \n", finalPrice);
-        printf("\t\t\t                           Customer         %s     \n", cust);
-        printf("\t\t\t                           Cashier          %s     \n", name);
-        printf("\t\t\t====================================================\n\n");
-        printf("\t\t\t\t\tEnd of bill\n\n");
+        printf("\t\t\t                                  ------------------------------\n\n");
+        printf("\t\t\t                                  SUBTOTAL          RM%10.2f     \n", subTotal);
+        printf("\t\t\t                                  GOVT. TAX (6%)    RM%10.2f     \n", tax);
+        printf("\t\t\t                                  TOTAL             RM%10.2f     \n", finalPrice);
+        printf("\t\t\t================================================================\n\n");
+        printf("\t\t\t\t\t\tEnd of Bill\n\n");
 
-        printf("Do you still want to continue? (y/n)");
+        printf("Do you still want to continue? (y/n)\n");
         scanf("%s", &userChoice);
     }
-
     return 0;
 }
